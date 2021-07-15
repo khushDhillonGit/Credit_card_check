c69f2019421c1f68d73e6b4930de7e3976896010
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2021-07-15 17:26:33 -0500

    checked for validity of credit card by using famous luhn's algorithm

--- credit.c
+++ credit.c
@@ -13,7 +13,62 @@ int main(void)
     // printf("%i",l);
     // check for valid length
 
+    if (valid_length(l))
+    {
 
+        int sum =0;
+        for(int i=0;c1!=0;i++,c1/=10)
+        {
+            if(i % 2 == 0)
+            {
+                sum += c1 % 10;
+            }
+            else
+            {
+                int k = 2*(c1 % 10);
+                sum += k / 10 + k % 10;
+            }
+
+        }
+
+        //formula
+        bool valid = false;
+        if (sum % 10 == 0)
+        {
+            valid = true;
+        }
+
+        //check for valid card number
+        if (valid)
+        {
+
+            if ((n >= 4e12 && n < 5e12) || (n >= 4e15 && n < 5e15))
+            {
+                printf("VISA");
+            }
+            else if (n >= 51e14 && n < 56e14)
+            {
+                printf("MASTERCARD\n");
+            }
+            else if ((n >= 34e13 && n < 35e13) || (n >= 37e13 && n < 38e13))
+            {
+                printf("AMEX\n");
+            }
+            else
+            {
+                printf("INVALID\n");
+            }
+
+        }
+        else
+        {
+            printf("INVALID\n");
+        }
+    }
+    else
+    {
+        printf("INVALID\n");
+    }
 
 
 
