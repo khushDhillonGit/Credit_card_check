f6d29941ba320a4dadc8db7a3cf041b576ba18a8
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2021-07-18 17:23:13 -0500

    initialised variables and included libraries

---
+++ credit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <cs50.h>
+
+bool valid_length(int n);
+int length(long n);
+int main(void)
+{
+    // get card no from user
+    long n = get_long("Enter credit card no: ");
+    long c1 = n;
+    // to find length
+    int l = length(n);
+    // printf("%i",l);
+    // check for valid length
+
+
+
+
+
+}
+
